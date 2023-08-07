234b23150b0b8ee2ccae9c2855e0c36d9b361cc5
Brian Worsham <brian.w.worsham@hotmail.com>
2023-08-07 08:44:04 -0500

    Add project files.

---
+++ Phase3FormSubmit/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Phase3FormSubmit.Models;
+
+namespace Phase3FormSubmit.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult FormGet()
+        {
+            ViewData["name"] = Request.Query["name"].ToString();
+            ViewData["email"] = Request.Query["email"].ToString();
+            ViewData["class"] = Request.Query["class"].ToString();
+            ViewData["address"] = Request.Query["address"].ToString();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FormPost(string name, string address, string email, string whichclass)
+        {
+            ViewData["name"] = name;
+            ViewData["email"] = email;
+            ViewData["class"] = whichclass;
+            ViewData["address"] = address;
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Phase3FormSubmit/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Phase3FormSubmit.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
